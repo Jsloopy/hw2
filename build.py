4e8c29776f481315582acc07102dc0d70f7f0b0b
Jordan Sloop <jsloop310@gmail.com>
2018-09-16 16:26:26 -0700

    working on homepage

--- build.py
+++ build.py
@@ -1,1 +1,11 @@
-build.py
+# First, get the template files
+top_template = open('templates/top.html').read()
+bottom_template = open('templates/bottom.html').read()
+
+# Read in index HTML code
+content = open('content/homepage1.html').read()
+
+# Combine template HTML code with content HTML code
+homepage_html = top_template + content + bottom_template
+open('homepage.html', 'w+').write(homepage_html)
+
