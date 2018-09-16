3f1d0fe990fee2c55dca17e02c7a48856d68c964
Jordan Sloop <jsloop310@gmail.com>
2018-09-16 16:35:04 -0700

    adding bio pg

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
-# First, get the template files
+
 top_template = open('templates/top.html').read()
 bottom_template = open('templates/bottom.html').read()
-
-# Read in index HTML code
 content = open('content/homepage1.html').read()
-
-# Combine template HTML code with content HTML code
 homepage_html = top_template + content + bottom_template
-open('homepage.html', 'w+').write(homepage_html)
+open('docs/homepage.html', 'w+').write(homepage_html)
 
+
+top_template = open('templates/top.html').read()
+bottom_template = open('templates/bottom.html').read()
+content = open('content/bio1.html').read()
+bio_html = top_template + content + bottom_template
+open('docs/bio.html', 'w+').write(bio_html)
