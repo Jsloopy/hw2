0767d7bf11be849b786f33cd9fe712a6ecd3f6de
Jordan Sloop <jsloop310@gmail.com>
2018-09-16 16:42:16 -0700

    adding contact pg to python script. Edited the resume script

--- build.py
+++ build.py
@@ -15,5 +15,11 @@ open('docs/bio.html', 'w+').write(bio_html)
 top_template = open('templates/top.html').read()
 bottom_template = open('templates/bottom.html').read()
 content = open('content/resume1.html').read()
-bio_html = top_template + content + bottom_template
-open('docs/resume.html', 'w+').write(bio_html)
+resume_html = top_template + content + bottom_template
+open('docs/resume.html', 'w+').write(resume_html)
+
+top_template = open('templates/top.html').read()
+bottom_template = open('templates/bottom.html').read()
+content = open('content/contact1.html').read()
+contact_html = top_template + content + bottom_template
+open('docs/contact.html', 'w+').write(contact_html)
