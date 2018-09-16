bf77d28a635231f244b67afcef7bcec32ebd556e
Jordan Sloop <jsloop310@gmail.com>
2018-09-16 16:37:44 -0700

    adding bio pg to puthon script

--- build.py
+++ build.py
@@ -11,3 +11,9 @@ bottom_template = open('templates/bottom.html').read()
 content = open('content/bio1.html').read()
 bio_html = top_template + content + bottom_template
 open('docs/bio.html', 'w+').write(bio_html)
+
+top_template = open('templates/top.html').read()
+bottom_template = open('templates/bottom.html').read()
+content = open('content/resume1.html').read()
+bio_html = top_template + content + bottom_template
+open('docs/resume.html', 'w+').write(bio_html)
