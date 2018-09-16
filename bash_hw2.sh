749b09ba67ebd5ca8ff1950b576b7d5184ef5bc8
Jordan Sloop <jsloop310@gmail.com>
2018-09-16 14:22:02 -0700

    adding bash script to repo

---
+++ bash_hw2.sh
@@ -0,0 +1,4 @@
+cat templates/top.html content/homepage1.html templates/bottom.html > docs/homepage.html
+cat templates/top.html content/resume1.html templates/bottom.html > docs/resume.html
+cat templates/top.html content/bio1.html templates/bottom.html > docs/bio.html
+cat templates/top.html content/contact1.html templates/bottom.html > docs/contact.html
