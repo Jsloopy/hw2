1cddeffde4b9e13b007ec888b4071dd32c77794a
Jordan Sloop <jsloop310@gmail.com>
2018-09-23 17:16:01 -0700

    adding for loop and list to def main() function section 2.2

--- build.py
+++ build.py
@@ -1,25 +1,56 @@
 
-top_template = open('templates/top.html').read()
-bottom_template = open('templates/bottom.html').read()
-content = open('content/homepage1.html').read()
-homepage_html = top_template + content + bottom_template
-open('docs/homepage.html', 'w+').write(homepage_html)
+# def main():
+#     top= open("templates/top.html").read()
+#     content= open("content/bio1.html").read()
+#     bottom= open("templates/bottom.html").read()
+
+# def main():
+#     top= open("templates/top.html").read()
+#     content= open("content/homepage1.html").read()
+#     bottom= open("templates/bottom.html").read()
+
+# def main():
+#     top= open("templates/top.html").read()
+#     content= open("content/contact1.html").read()
+#     bottom= open("templates/bottom.html").read()
+
+# def main():
+#     top= open("templates/top.html").read()
+#     content= open("content/resume1.html").read()
+#     bottom= open("templates/bottom.html").read()
 
 
-top_template = open('templates/top.html').read()
-bottom_template = open('templates/bottom.html').read()
-content = open('content/bio1.html').read()
-bio_html = top_template + content + bottom_template
-open('docs/bio.html', 'w+').write(bio_html)
+pages = [
+    {
+        "filename" : "content/homepage1.html",
+        "output" : "docs/homepage.html",
+        "title" : "My Homepage",
+    },
+    {
+        "filename" : "content/bio1.html",
+        "output" : "docs/bio.html",
+        "title" : "Bio",
+    },
+    {
+        "filename" : "content/resume1.html",
+        "output" : "docs/resume.html",
+        "title" : "Resume",
+    },
+    {
+       "filename" : "content/contact1.html",
+        "output" : "docs/contact.html",
+        "title" : "Contact Me", 
+    },
+]
+def main():
+    for value in pages:
+        top_template = open('templates/top.html').read()
+        bottom_template = open('templates/bottom.html').read()
+        content = open(value["filename"]).read()
+        homepage_html = top_template + content + bottom_template
+        open(value["output"], 'w+').write(homepage_html)
 
-top_template = open('templates/top.html').read()
-bottom_template = open('templates/bottom.html').read()
-content = open('content/resume1.html').read()
-resume_html = top_template + content + bottom_template
-open('docs/resume.html', 'w+').write(resume_html)
 
-top_template = open('templates/top.html').read()
-bottom_template = open('templates/bottom.html').read()
-content = open('content/contact1.html').read()
-contact_html = top_template + content + bottom_template
-open('docs/contact.html', 'w+').write(contact_html)
+
+if __name__ == "__main__":
+    main()
