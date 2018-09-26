30c25a73ed285d2d934f704f18fb03d55a7c34c9
Jordan Sloop <jsloop310@gmail.com>
2018-09-25 21:54:39 -0700

    finalized 2.4 function refactor

--- build.py
+++ build.py
@@ -2,12 +2,12 @@
 pages = [
     {
         "filename" : "content/homepage1.html",
-        "output" : "docs/homepage2S.html",
+        "output" : "docs/homepage3.html",
         "title" : "My Homepage",
     },
     {
         "filename" : "content/bio1.html",
-        "output" : "docs/bio.html",
+        "output" : "docs/bio3.html",
         "title" : "Bio",
     },
     {
@@ -47,17 +47,19 @@ pages = [
 # # # #2.4 Function refactor
 
 def apply_template(value):
-    for value in pages:
-        template = open("templates/base.html").read()
-        content = open(value["filename"]).read()
-        index_html= template.replace("{{content}}", content)
-        return (index_html)
+    template = open("templates/base.html").read()
+    # print(value)
+    index_html= template.replace("{{content}}", value)
+    return (index_html)
 
 def main():
     for value in pages:
-        content = open(value["output"]).read
+        content = open(value["filename"]).read()
+        #content = open('content/homepage1.html').read()
         resulting_html_for_index = apply_template(content)
-		    open(value["output"], "w+").write(resulting_html_for_index)
+        print("////////////////////////////")
+        print(resulting_html_for_index)
+        open(value["output"], "w+").write(resulting_html_for_index)
     
 
 if __name__ == "__main__":
