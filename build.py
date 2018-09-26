e25d2dbc9e2166e634dfe0de212d0428843d6522
Jordan Sloop <jsloop310@gmail.com>
2018-09-25 20:32:48 -0700

    workign on 2.4

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 pages = [
     {
         "filename" : "content/homepage1.html",
-        "output" : "docs/homepage.html",
+        "output" : "docs/homepage2S.html",
         "title" : "My Homepage",
     },
     {
@@ -21,23 +21,44 @@ pages = [
         "title" : "Contact Me", 
     },
 ]
-def main():
-    for value in pages:
-        top_template = open('templates/top.html').read()
-        bottom_template = open('templates/bottom.html').read()
-        content = open(value["filename"]).read()
-        homepage_html = top_template + content + bottom_template
-        open(value["output"], 'w+').write(homepage_html)
+# def main():
+#     for value in pages:
+#         top_template = open('templates/top.html').read()
+#         bottom_template = open('templates/bottom.html').read()
+#         content = open(value["filename"]).read()
+#         homepage_html = top_template + content + bottom_template
+#         open(value["output"], 'w+').write(homepage_html)
 
 
 
-if __name__ == "__main__":
-    main()
 
-#2.3 string replacement 
-def meain():
+
+# # 2.3 string replacement 
+# def main():
+#     for value in pages:
+#         template = open("templates/base.html").read()
+#         content = open(value["filename"]).read()
+#         index_html= template.replace("{{content}}", content)
+#         open(value["output"], 'w+').write(index_html)
+
+
+
+
+# # # #2.4 Function refactor
+
+def apply_template(value):
     for value in pages:
         template = open("templates/base.html").read()
         content = open(value["filename"]).read()
-        homepage_html= template.replace("{{content}}", content)
-        open(value["output"], 'w+').write(homepage_html)
+        index_html= template.replace("{{content}}", content)
+        return (index_html)
+
+def main():
+    for value in pages:
+        content = open(value["output"]).read
+        resulting_html_for_index = apply_template(content)
+		    open(value["output"], "w+").write(resulting_html_for_index)
+    
+
+if __name__ == "__main__":
+    main()
