dd6b0722dedc4f930c9978a985de954412e7f222
Jordan Sloop <jsloop310@gmail.com>
2018-09-30 14:58:56 -0700

    Solving phase 2.1

--- build.py
+++ build.py
@@ -8,28 +8,28 @@
 #         open(value["output"], 'w+').write(homepage_html)
 
 
-pages = [
-    {
-        "filename" : "content/homepage1.html",
-        "output" : "docs/homepage.html",
-        "title" : "My Homepage",
-    },
-    {
-        "filename" : "content/bio1.html",
-        "output" : "docs/bio.html",
-        "title" : "Bio",
-    },
-    {
-        "filename" : "content/resume1.html",
-        "output" : "docs/resume.html",
-        "title" : "Resume",
-    },
-    {
-       "filename" : "content/contact1.html",
-        "output" : "docs/contact.html",
-        "title" : "Contact Me", 
-    },
-]
+# pages = [
+#     {
+#         "filename" : "content/homepage1.html",
+#         "output" : "docs/homepage.html",
+#         "title" : "My Homepage",
+#     },
+#     {
+#         "filename" : "content/bio1.html",
+#         "output" : "docs/bio.html",
+#         "title" : "Bio",
+#     },
+#     {
+#         "filename" : "content/resume1.html",
+#         "output" : "docs/resume.html",
+#         "title" : "Resume",
+#     },
+#     {
+#        "filename" : "content/contact1.html",
+#         "output" : "docs/contact.html",
+#         "title" : "Contact Me", 
+#     },
+# ]
 
 # 2.2
 # def main():
@@ -69,8 +69,41 @@ pages = [
 #         open(value["output"], "w+").write(resulting_html_for_index)
     
 
-# # # # #2.5- advanced templating
+# # # # # Start of homework 4
 
+# HMWRK4 2.1
+
+#this grabs all of the content files
+import glob
+import os
+all_html_files = glob.glob("content/*.html")
+print(all_html_files)
+
+# #this grabs the name only of the output docs. USe for grabbing titles 
+
+file_path = "content/blog.html"
+
+
+pages = []
+def html_output():
+    for each in all_html_files:
+        print(each)
+        file_name = os.path.basename(each)
+        print(file_name)
+        name_only, extension = os.path.splitext(file_name)
+        print(name_only)
+        output = 'docs/' + name_only + '.html'
+        print(output)
+
+        pages.append({
+            "filename": each ,
+            "title": name_only,
+            "output": output,
+        }) 
+    
+
+html_output()
+print(pages)
 
 def apply_template(value, title):
     template = open("templates/base.html").read()
